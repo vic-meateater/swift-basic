1296ff51d4187355d919e08edf3cd97955d15bdc
Vic <meateater.jobs@gmail.com>
2020-11-19 16:47:43 +0500

    Сделал структуры.

--- 3l_МясоедовВиктор.playground/Contents.swift
+++ 3l_МясоедовВиктор.playground/Contents.swift
@@ -1,3 +1,40 @@
 import UIKit
 
-var str = "Hello, playground"
+//Описать несколько структур – любой легковой автомобиль SportCar и любой грузовик TrunkCar.
+//Структуры должны содержать марку авто, год выпуска, объем багажника/кузова, запущен ли двигатель, открыты ли окна, заполненный объем багажника.
+//Описать перечисление с возможными действиями с автомобилем: запустить/заглушить двигатель, открыть/закрыть окна, погрузить/выгрузить из кузова/багажника груз определенного объема.
+//Добавить в структуры метод с одним аргументом типа перечисления, который будет менять свойства структуры в зависимости от действия.
+//Инициализировать несколько экземпляров структур. Применить к ним различные действия.
+
+enum Engine {
+    case start, stop
+}
+
+enum Windows {
+    case open, close
+}
+
+enum Trunk {
+    case load, unload
+}
+
+struct SportCar {
+    let brand: String
+    let yearOfProduction: Int
+    let FullTrunkVolume: Int
+    let TrunkVolume: Int
+    var windowsState: Windows
+    var engineState: Engine
+    
+    
+    mutating func closeWindows(){
+        
+    }
+}
+
+struct TrunkCar {
+    let brand: String
+    let yearOfProduction: Int
+    let FullTrunkVolume: Int
+    let TrunkVolume: Int
+}
